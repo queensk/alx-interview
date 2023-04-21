775b3329cd18e0cdfeed30b08e2d65e6fecc4030
queensk <queenskisivuli@gmail.com>
2023-04-21 08:10:37 +0300

    fix pycode syle line too long

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Create a function def pascal_triangle(n):
-that returns a list of lists of integers representing the Pascal’s triangle of n:
+that returns a list of lists of integers 
+representing the Pascal’s triangle of n:
 """
 
 
