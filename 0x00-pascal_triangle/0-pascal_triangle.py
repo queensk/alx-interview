27f17f4fc944885306af2aa414dcb664cc042b57
queensk <queenskisivuli@gmail.com>
2023-04-21 08:13:59 +0300

    fix trailing whitespace

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Create a function def pascal_triangle(n):
-that returns a list of lists of integers 
+that returns a list of lists of integers
 representing the Pascal’s triangle of n:
 """
 
