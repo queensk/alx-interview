dedcaf96c0399a4fc0fb22977cad223b81e01c96
queensk <queenskisivuli@gmail.com>
2023-06-02 01:02:21 +0300

    semistandard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,13 +2,13 @@
 const request = require('request');
 
 // Define a helper function that returns a promise for a request
-const requestPromise = async (value) => 
-    new Promise((resolve, reject) => {
-        request(value, (error, response, data) => {
-            if(error) reject(error)
-            else resolve(data)
-        })
+const requestPromise = async (value) =>
+  new Promise((resolve, reject) => {
+    request(value, (error, response, data) => {
+      if (error) reject(error);
+      else resolve(data);
     });
+  });
 
 const endpoint = 'https://swapi-api.hbtn.io/api';
 const filmId = process.argv[2];
@@ -16,10 +16,10 @@ const filmId = process.argv[2];
 request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
   if (error) return console.log(error);
 
-  let characters = JSON.parse(body).characters;
+  const characters = JSON.parse(body).characters;
 
   for (const character of characters) {
-    let data = await requestPromise(character);
+    const data = await requestPromise(character);
     console.log(JSON.parse(data).name);
   }
 });
