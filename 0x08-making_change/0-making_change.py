27c63c4ba1b65066a72704ec05ac663138d09f3b
queensk <queenskisivuli@gmail.com>
2023-06-17 18:49:30 +0300

    make change function

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+"""
+def makeChange(coins, total):
+    """
+    Return: fewest number of coins needed to meet total
+    """
+    if total <= 0:
+        return 0
+    
+    coins.sort(reverse = True)
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
