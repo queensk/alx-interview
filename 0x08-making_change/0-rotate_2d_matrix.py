e32ba7f058f8d828dee3ff4440eab4a4beeb1b4d
queensk <queenskisivuli@gmail.com>
2023-06-19 02:58:23 +0300

    rotate 2d

---
+++ 0x08-making_change/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a matrix by 90 degrees clockwise,
+    """
+    # transpose the matrix
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # reverse each row
+    for row in matrix:
+        row.reverse()
